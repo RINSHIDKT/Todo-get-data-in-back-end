0f2ea61c9305eeb10c267f96926fb7a106c51a80
RINSHIDKT <rinshidkt6@gmail.com>
2023-10-19 10:17:36 +0530

    file added

---
+++ controller.js
@@ -0,0 +1,8 @@
+import schema from './user.model.js'
+export async function addTask(req,res)
+
+{
+    const {task}=req.body
+    console.log(task);
+    res.status(201).send(schema.create({task}));
+}
---
+++ router.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import * as  controller from "./controller.js"
+const router=Router();
+router.route("/addtask").post(controller.addTask)
+
+export default router;
